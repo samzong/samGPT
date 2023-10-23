dfb46b37d1bcdd7d16945b00407812fa2139c5c3
samzong.lu <samzong.lu@gmail.com>
2023-10-23 17:47:36 +0800

    Initial commit

---
+++ samgpt/__init__.py
@@ -0,0 +1,12 @@
+"""samgpt package."""
+from importlib import metadata
+
+from samgpt.chain import get_chain
+
+try:
+    __version__ = metadata.version(__package__)
+except metadata.PackageNotFoundError:
+    # Case where package metadata is not available.
+    __version__ = ""
+
+__all__ = [__version__, "get_chain"]
